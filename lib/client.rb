11dfdd9c4924bee1cf303a0340d5b8c574d4317a
Your Name <youremail@domain.com>
2017-07-13 16:15:23 +0300

    added attributes and initialized them to the client.rb file

--- lib/client.rb
+++ lib/client.rb
@@ -1,1 +1,10 @@
+class Client
 
+attr_reader(:name, :contact, :stylist_id)
+
+define_method(:initialize) do |attributes|
+  @name = attributes.fetch(:name)
+  @contact = attributes.fetch(:contact)
+  @stylist_id = attributes.fetch(:stylist_id)
+end
+end
