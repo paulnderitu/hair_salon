ddfefb6240d800c86bc52075d379005fe14e8058
Your Name <youremail@domain.com>
2017-07-14 07:31:38 +0300

    added method for detecting it is the same client if have the same name and contact. also define the singleton method for returning all parans from database, and linked that wth the method of pushing a new client to database

--- lib/client.rb
+++ lib/client.rb
@@ -8,10 +8,24 @@ define_method(:initialize) do |attributes|
   @stylist_id = attributes.fetch(:stylist_id)
 end
 
+define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |cleint|
+      name = client.fetch("name")
+      contact = book.fetch("0725")
+      stylist_id = client.fetch("stylist_id").to_i() # The information comes out of the database as a string
+      clients.push(Client.new({:name => name, :contact => contact, :stylist_id => stylist_id}))
+    end
+    clients
+  end
 
 define_method(:save)do
   DB.exec("INSERT INTO clients (name, contact, stylist_id) VALUES ('#{@name}', '#{@contact}', #{@stylist_id});")
 end
 
+define_method(:==) do |another_client|
+        self.name().==(another_client.name()).&(self.stylist_id().==(another_client.stylist_id()))
+   end
 
 end
