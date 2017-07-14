78a02bb390ec2ea921c2e836bce89fff31d72a15
Your Name <youremail@domain.com>
2017-07-14 07:38:35 +0300

    added method for adding a new stylist to the database

---
+++ lib/stylist.rb
@@ -0,0 +1,16 @@
+class Stylist
+
+attr_reader(:name, :id)
+
+define_method(:initialize) do |attributes|
+  @name = attributes.fetch(:name)
+  @id = attributes.fetch(:id)
+end
+
+define_method(:save) do
+      result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+      @id = result.first().fetch("id").to_i()
+    end
+
+
+end
