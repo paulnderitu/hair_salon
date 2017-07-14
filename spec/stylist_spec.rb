7c64dd0a7440c65a936428600b17dfa04f22091c
Your Name <youremail@domain.com>
2017-07-14 07:43:31 +0300

    added specs for adding a stylist id and name on save

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -10,4 +10,19 @@ it("lets you save a new stylist to the database")do
 end
 end
 
+describe("#name")do
+  it("tells you the name of the stylist") do
+    stylist = Stylist.new({:name => "betty", :id => nil})
+    expect(stylist.name()).to(eq("betty"))
+  end
+end
+
+describe("#id")do
+  it("sets the stylist ID when a new stylist added") do
+    stylist = Stylist.new({:name => "betty", :id => nil})
+    stylist.save()
+    expect(stylist.id()).to(be_an_instance_of(Fixnum))
+  end
+end
+
 end
