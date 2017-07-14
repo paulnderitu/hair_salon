409480e141836c89f46f77958334a5d0bb33c13f
Your Name <youremail@domain.com>
2017-07-14 07:55:59 +0300

    added method for fetching stylists params and specific clients abnd pushing them to their respective arrays in the database

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -7,6 +7,19 @@ define_method(:initialize) do |attributes|
   @id = attributes.fetch(:id)
 end
 
+
+define_method(:stylists) do
+  stylist_clients = []
+  clients = DB.exec("SELECT * FROM clientss WHERE stylist_id = #{self.id()};")
+      clients.each() do |client|
+        name = client.fetch("name")
+        contact = contact.fetch("contact")
+        stylist_id = client.fetch("stylist_id").to_i()
+        stylist_clients.push(Client.new({:name => name, :contact => contact, :stylist_id => stylist_id}))
+      end
+      stylist_clients
+end
+
 define_method(:save) do
       result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
       @id = result.first().fetch("id").to_i()
